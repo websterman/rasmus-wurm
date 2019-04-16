cc9a9e8796dc701949760972831cda58c3e4222f
Karl Westerman <karl@websterman.se>
2019-04-16 15:41:07 +0200

    bubbles

--- src/pages/for-vuxna.js
+++ src/pages/for-vuxna.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, graphql } from "gatsby"
 import styled from "@emotion/styled"
 import { css } from "@emotion/core"
@@ -6,6 +6,7 @@ import Header from '../components/header';
 import BG from '../images/Bokeh_vuxen.jpg';
 import curtains from '../images/curtains.jpg';
 import Footer from '../components/footer';
+import BubbleImg from '../images/bubble.png';
 
 const TextBox = styled.div`
 color: white;
@@ -16,7 +17,7 @@ color: white;
     
 `;
 
-const IntroBox = styled.div `
+const IntroBox = styled.div`
     background-image: url(${curtains});
     height: 0;
     position: relative;
@@ -33,52 +34,79 @@ const IntroBox = styled.div `
     }
 
 `;
-    
-const Row = styled.div `
+
+const Row = styled.div`
     position: relative;
     display: flex;
     align-items: stretch;
     `;
 
 
-const OrderLeft = css `
+const OrderLeft = css`
     order: 1;
     width: 50%;`
 
-const OrderRight = css `
+const OrderRight = css`
     order: 2;
     width: 50%;
 `
-const AdultImg = styled.img `
+const AdultImg = styled.img`
     width: 100%;
     margin: 0;
     display: block;`;
+const ForAdults = ({ data }) => {
+  var parallaxEls;
+  useEffect(() => {
 
-    const ForAdults = ({ data }) => {
-    console.log(data.wordpressPage)
+    parallaxEls = document.querySelectorAll("[data-speed]");
+    window.addEventListener("scroll", scrollHandler);
+
+  }, [])
+
+
+  function scrollHandler() {
+
+    for (const parallaxEl of parallaxEls) {
+      const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
+      const transformY = this.pageYOffset * parallaxEl.dataset.speed;
+      if (parallaxEl.classList.contains("banner-title")) {
+        parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
+      } else if (parallaxEl.classList.contains("banner-subtitle")) {
+        parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
+      } else {
+        parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+      }
+    }
+  }
+
+  const Bubble = styled.img`
+    opacity: .15;
+    position: absolute;
+    top: -60px;`
   return (
     <div>
-        <Header />
-        <IntroBox className="intro-container"><div className = "IntroText">
+      <Header />
+      <IntroBox className="intro-container"><div className="IntroText">
         <h2>{data.wordpressPage.acf.intro.intro_titel}</h2>
-        <p dangerouslySetInnerHTML={{__html: data.wordpressPage.acf.intro.intro_content}} />
-        </div>
-        </IntroBox>
+        <p dangerouslySetInnerHTML={{ __html: data.wordpressPage.acf.intro.intro_content }} />
+      </div>
+      </IntroBox>
 
-        {data.wordpressPage.acf.row.map( (aRow, index) => {
-            let imgClass = OrderLeft;
-            let textClass = OrderRight;
-            console.log(aRow);
-            if( index %2 === 1) {
-                imgClass = OrderRight;
-                textClass = OrderLeft;
-            }
-            return <Row key={index} className="adult-box">
-            <div css={imgClass} className="image-container"><AdultImg src={aRow.row_image.localFile.childImageSharp.original.src} /></div><TextBox css={textClass}><div><h1>{aRow.row_text.row_title}</h1><p dangerouslySetInnerHTML={{__html: aRow.row_text.row_content}}/></div></TextBox></Row>
-        })}
-        <Footer />
- </div>
-    )
+      {data.wordpressPage.acf.row.map((aRow, index) => {
+        let imgClass = OrderLeft;
+        let textClass = OrderRight;
+        console.log(aRow);
+        if (index % 2 === 1) {
+          imgClass = OrderRight;
+          textClass = OrderLeft;
+        }
+        const bubbleNo = [0, 1, 2, 3, 4,5 ,6,7,8,9,10];
+        return <Row key={index} className="adult-box">{bubbleNo.map(b => { return <Bubble key={b} className="bubbl" style={{ width: 3 + Math.random() * 6 + "%", left: `${Math.random() * 100}%`, top: `${Math.random() * 100}%` }} data-speed={+ Math.random().toFixed(1) * .6} data-direction="up" src={BubbleImg} /> })}
+          <div css={imgClass} className="image-container"><AdultImg src={aRow.row_image.localFile.childImageSharp.original.src} /></div><TextBox css={textClass}><div><h1>{aRow.row_text.row_title}</h1><p dangerouslySetInnerHTML={{ __html: aRow.row_text.row_content }} /></div></TextBox></Row>
+      })}
+      <Footer />
+    </div>
+  )
 }
 export default ForAdults;
 
